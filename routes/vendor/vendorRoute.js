9b39c19cf3c64e48c01ff4069678ab40667262c8
sensitive01 <tech@sensitive.co.in>
2025-01-27 18:16:49 +0530

    Update vendorRoute.js

--- routes/vendor/vendorRoute.js
+++ routes/vendor/vendorRoute.js
@@ -66,7 +66,7 @@ vendorRoute.put("/approvebooking/:id", bookingController.updateApproveBooking);
 vendorRoute.put("/cancelbooking/:id", bookingController.updateCancelBooking);
 vendorRoute.put("/approvedcancelbooking/:id", bookingController.updateApprovedCancelBooking);
 vendorRoute.put("/allowparking/:id", bookingController.allowParking);
-
+vendorRoute.put("/usercancelbooking/:id", bookingController.userupdateCancelBooking);
 
 vendorRoute.get("/fetchbookingsbyvendorid/:id", fetchbyidController.fetchBookingsByVendorId);
 
