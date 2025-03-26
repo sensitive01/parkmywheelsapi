c8cfe5df8ebf21f77cded2aad3a9d2d45cbf29e0
Pujasmn <vaktisa2000@gmail.com>
2025-03-26 17:54:52 +0530

    add

---
+++ models/planSchema.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const PlanSchema = new mongoose.Schema({
+  planName: {
+    type: String,
+  },
+  validity: {
+    type: String,
+  },
+  amount: {
+    type: String,
+  },
+  features: [{
+    type: String,
+  }],
+  status: {
+    type: String,
+  },
+  image: {
+    type: String, 
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Plan', PlanSchema);
