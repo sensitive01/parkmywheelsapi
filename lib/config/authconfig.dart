7bc48b1a7b15b453dcfc18b5123822a7a68c9025
Jeyaram A <ramram13052001@gmail.com>
2025-10-31 10:34:57 +0530

    now

--- lib/config/authconfig.dart
+++
@@ -1,9 +0,0 @@
-// config.dart
-
-class ApiConfig {
-  // static const String baseUrl = 'https://parkmywheelsapi.onrender.com/';
-  static const String baseUrl = 'https://api.parkmywheels.com/';
-
-  // static const String baseUrl = 'http://localhost:4000/';
-
-}
